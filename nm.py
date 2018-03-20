90ca43a5ce28a6bb12e9e837d54929c4757c5804
MMace <35781354+Matthew-Mace@users.noreply.github.com>
2018-03-20 13:54:16 +0000

    umm

---
+++ nm.py
@@ -0,0 +1,6 @@
+import time
+aye = "hello"
+print ("Yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+time.sleep(2)
+while aye == "hello":
+    print("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
