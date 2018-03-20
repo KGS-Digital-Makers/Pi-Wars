a1fea8fa41c9d849b8eba28dc91abe43ae1b227b
MMace <35781354+Matthew-Mace@users.noreply.github.com>
2018-03-20 13:54:51 +0000

    begone foul demon

--- nm.py
+++
@@ -1,6 +0,0 @@
-import time
-aye = "hello"
-print ("Yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
-time.sleep(2)
-while aye == "hello":
-    print("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
